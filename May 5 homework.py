bfea0afe0650cc074c955d836d496fd8b158decd
Jabah Gundy <gundy157@outlook.com>
2021-05-06 11:22:53 -0400

    Updated homework

--- May 5 homework.py
+++ May 5 homework.py
@@ -1,19 +1,27 @@
-day = int(input("Give me a number 0-7 and I'll tell you what day it is: "))
+day = int(input('Give me a number 0-7 and I\'ll tell you what day it is: '))
 
-if day == 1:
-    print("Monday")
-if day == 2:
-    print ("Tuesday")
-if day == 3:
-    print("Wednesday")
-if day == 4:
-    print("Thursday")
-if day == 5:
-    print("Friday")
-if day == 6:
-    print("Saturday")
-if day == 7:
-    print("Sunday")
+if day == 0:
+    print('Not a day')
+elif day == 1:
+    print('Monday')
+elif day == 2:
+    print ('Tuesday')
+elif day == 3:
+    print('Wednesday')
+elif day == 4:
+    print('Thursday')
+elif day == 5:
+    print('Friday')
+elif day == 6:
+    print('Saturday')
+elif day == 7:
+    print('Sunday')
 
 else:
-    print("Please give me a valid number")
+    print('Please give me a valid number')
+
+if day >= 1 and day <= 5:
+    print('It\'s time to go to work')
+if day == 6 or day == 7:
+    print('It\'s the weekend, sleep in')
+    
